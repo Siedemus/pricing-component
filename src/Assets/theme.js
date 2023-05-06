bd97bc7668c3c7f815d13b29e5d96e48f7a21750
Bartosz Sakiewa <barsak1177@gmail.com>
2023-05-06 03:39:50 +0200

    add assets folder and theme

---
+++ src/Assets/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+    colors: {
+        veryLightBlue: 'hsl(240, 78%, 98%)',
+        lightBlue: 'hsl(234, 14%, 74%)',
+        blue: 'hsl(233, 13%, 49%)',
+        darkBlue: 'hsl(232, 13%, 33%)',
+    },
+    gradients: {
+        basicGradient: 'linear-gradient(to right, hsl(236, 72%, 79%), hsl(237, 63%, 64%))',
+    },
+    sizes: {
+        basicFS: '15px',
+    },
+    fonts: {
+        montS: 'Montserrat, sans-serif',
+    },
+};
