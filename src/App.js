2ff42388842b6944d44a171c7740f1749b2218ad
Bartosz Sakiewa <barsak1177@gmail.com>
2023-05-06 21:41:12 +0200

    move toggleChecked and isChecked state to app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import { Switch } from "./Switch";
 import { Offers } from "./Offers";
 import { Background } from "./Background";
 import { Footer } from "./Footer";
+import { useState } from "react";
 
 function App() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const toggleChecked = () => {
+    setIsChecked((isChecked) => !isChecked);
+  };
+
   return (
     <>
       <Header />
-      <Switch />
+      <Switch toggleChecked={toggleChecked} isChecked={isChecked} />
       <Offers />
       <Background />
       <Footer />
--- src/Switch/index.js
+++ src/Switch/index.js
@@ -6,20 +6,12 @@ import {
 } from "./styled";
 import { useState } from "react";
 
-export const Switch = () => {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleSwitch = () => {
-    setIsChecked((isChecked) => !isChecked);
-  };
-
+export const Switch = ({ toggleChecked, isChecked }) => {
   return (
     <StyledContainer>
       <StyledOption>Annually</StyledOption>
-      <SwitchContainer onClick={handleSwitch}>
-        <SwitchSlider isChecked={isChecked}>
-
-        </SwitchSlider>
+      <SwitchContainer onClick={toggleChecked}>
+        <SwitchSlider isChecked={isChecked}></SwitchSlider>
       </SwitchContainer>
       <StyledOption>Monthly</StyledOption>
     </StyledContainer>
