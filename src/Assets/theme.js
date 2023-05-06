f4da53b62222da99e1bf8af3304a719ad8f81baa
Bartosz Sakiewa <barsak1177@gmail.com>
2023-05-06 20:41:03 +0200

    add gradients and color

--- src/Assets/theme.js
+++ src/Assets/theme.js
@@ -4,9 +4,13 @@ export const theme = {
         lightBlue: 'hsl(234, 14%, 74%)',
         blue: 'hsl(233, 13%, 49%)',
         darkBlue: 'hsl(232, 13%, 33%)',
+        white: "#fff",
     },
     gradients: {
         basicGradient: 'linear-gradient(to right, hsl(236, 72%, 79%), hsl(237, 63%, 64%))',
+        bttnGradient: 'linear-gradient(107deg, rgba(163,168,240,1) 3%, rgba(108,114,222,1) 44%, rgba(156,161,238,1) 72%, rgba(105,111,221,1) 93%)',
+        secondBttnGradient: 'linear-gradient(107deg, rgba(241,242,255,1) 21%, rgba(255,255,255,1) 93%)',
+        whiteGradient: "linear-gradient(107deg, rgba(255,255,255,1) 11%, rgba(194,194,194,1) 54%, rgba(204,204,204,1) 56%, rgba(255,255,255,1) 100%)",
     },
     sizes: {
         basicFS: '15px',
